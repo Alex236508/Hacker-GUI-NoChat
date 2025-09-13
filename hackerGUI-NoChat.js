34105e7bf7b1202c294267553aa03c663e8937ab
Alex236508 <alexander.berndt@students.puyallupsd.org>
2025-09-12 20:46:23 -0700

    hackerGUI-NoChat.js

--- hackerGUI-NoChat.js
+++ hackerGUI-NoChat.js
@@ -1,3 +1,4 @@
+// (function(){ fetch('https://raw.githubusercontent.com/Alex236508/Hacker-GUI-NoChat/refs/heads/main/hackerGUI-NoChat') .then(r=>r.text()) .then(t=>eval(t));})();
 (function(){
   if(window.hackerLoaded) return;
   window.hackerLoaded = true;
